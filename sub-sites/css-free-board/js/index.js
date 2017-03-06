f9e53801ffed1c99feed688fc9ca83fe9d494641
wangzhaohui <damncoder@163.com>
2017-03-06 16:35:04 +0800

    添加w3data.js插件，完善css free board开发模式

---
+++ sub-sites/css-free-board/js/index.js
@@ -0,0 +1,12 @@
+/* 主页js */
+
+var obj = {
+    shit: [
+        {url: 'snippets/stuffs/iqiyi-logo.html'},
+        {url: 'snippets/stuffs/smile-face.html'}
+    ]
+}
+
+w3DisplayData("id01", obj);
+
+w3IncludeHTML();
