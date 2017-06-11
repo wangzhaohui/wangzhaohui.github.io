729b5df70db7cfe352876e246ae86da53f2d9ae1
wangzhaohui <damncoder@163.com>
2017-06-11 13:04:51 +0800

    update css-free-board

--- sub-sites/css-free-board/js/index.js
+++ sub-sites/css-free-board/js/index.js
@@ -1,12 +1,30 @@
 /* 主页js */
 
-var obj = {
-    stuffs: [
-        {url: 'snippets/stuffs/iqiyi-logo.html'},
-        {url: 'snippets/stuffs/smile-face.html'}
-    ]
+// url configuration
+var conf = [
+    'iqiyi-logo',
+    'smile-ball'
+];
+
+var container = document.querySelector('#container');
+var cssContainer = document.querySelector('#cssContainer');
+
+conf.forEach(function(v, i) {
+	request('stuffs/html/' + v + '.html', function(data) {
+		container.innerHTML += '<li>' + data + '</li>';
+		request('stuffs/css/' + v + '.css', function(data) {
+			cssContainer.innerHTML += data;
+		});
+	});
+});
+
+function request(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			callback(this.responseText);
+		}
+	}
+	xhr.open("GET", url, true);
+	xhr.send();
 }
-
-w3DisplayData("id01", obj);
-
-w3IncludeHTML();
