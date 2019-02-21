cccb8d9b4a64d159dbb72c104f5f8b10b29c8bac
王昭辉 <wangzhaohui@qiyi.com>
2019-02-21 21:22:23 +0800

    add edge check and fill rects

--- js-src/index.js
+++ js-src/index.js
@@ -5,13 +5,15 @@
   const ctx = canvas.getContext('2d');
 
   class Rect {
-    constructor(lineWidth, strokeStyle, x, y, w, h) {
+    constructor(lineWidth, strokeStyle, x, y, w, h, directX, directY) {
       this.lineWidth = lineWidth;
       this.strokeStyle = strokeStyle;
       this.x = x;
       this.y = y;
       this.w = w;
       this.h = h;
+      this.directX = directX;
+      this.directY = directY;
     }
     get pos() {
       return [this.x, this.y];
@@ -20,45 +22,81 @@
       this.x = pos[0];
       this.y = pos[1];
     }
+    turnXReverse() {
+      this.directX *= -1;
+    }
+    turnYReverse() {
+      this.directY *= -1;
+    }
   }
 
+  // Generate rects
   const rects = [];
   for(let i = 0; i < 20; i++) {
-    const w = getRandom(50, 100);
-    const h = getRandom(50, 100);
+    const length = Math.min(canvas.width, canvas.height);
+    const w = getRandom(length * 0.1, length * 0.3);
+    const h = getRandom(length * 0.1, length * 0.3);
     rects.push(new Rect(
       '1',
-      getRandomRgba(),
+      getRandomRgba(30, 220),
       getRandom(0, canvas.width - w),
       getRandom(0, canvas.height - h),
       w,
-      h
+      h,
+      getRandomFrom([-1, 1]),
+      getRandomFrom([-1, 1])
     ));
   }
 
+  // Motion
   setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     rects.forEach((rect) => {
       const oldPos = rect.pos;
-      rect.pos = [oldPos[0] + getRandom(-1, 2), oldPos[1] + getRandom(-1, 2)];
+      rect.pos = [oldPos[0] + rect.directX * 1, oldPos[1] + rect.directY * 1];
+
+      const result = checkEdge(rect);
+      if (result.x) {
+        rect.turnXReverse();
+      }
+      if (result.y) {
+        rect.turnYReverse();
+      }
       drawRect(rect);
     });
-  }, /* 41.67 */10);
-
+  }, 30);
+  
   function drawRect(rect) {
     ctx.beginPath();
     ctx.lineWidth = rect.lineWidth;
     ctx.strokeStyle = rect.strokeStyle;
     ctx.rect(rect.x, rect.y, rect.w, rect.h);
     ctx.stroke();
+
+    ctx.fillStyle = rect.strokeStyle;
+    ctx.fill();
+  }
+
+  // Motion edge check
+  function checkEdge(rect) {
+      return {
+        x: rect.x <= 0 || rect.x + rect.w >= canvas.width,
+        y: rect.y <= 0 || rect.y + rect.h >= canvas.height
+      };
   }
 
   function getRandom(min, max) {
     return Math.floor(min + Math.random() * (max - min));
   }
 
-  function getRandomRgba() {
-    return `rgba(${getRandom(0, 255)},${getRandom(0, 255)},${getRandom(0, 255)},1)`;
+  function getRandomRgba(min = 0, max = 255, alpha = 1) {
+    return `rgba(${getRandom(min, max)},${getRandom(min, max)},${getRandom(min, max)},${alpha})`;
+  }
+
+  function getRandomFrom(arr) {
+    const len = arr.length;
+    var num = getRandom(0, len);
+    return arr[num];
   }
 
 })(document);
